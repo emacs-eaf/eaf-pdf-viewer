726db97efc2a4368a7e649ff9aa877930ac2fe46
czqhurricnae <czqhurricane@gmail.com>
2022-12-08 17:35:57 +0800

    Update eaf-pdf-viewer.el

--- eaf-pdf-viewer.el
+++ eaf-pdf-viewer.el
@@ -722,18 +722,18 @@ This function works best if paired with a fuzzy search package."
   (let* ((page-number (or (eaf-call-sync "execute_function" eaf--buffer-id "current_page") "1")))
     (do-applescript
      (concat "tell application \"Adobe Acrobat\"\n"
-             "	try\n"
-     (format "		open \"%s\" options \"page=%s\"\n" eaf--buffer-url page-number)
-             "	on error\n"
-     (format "		display alert \"Cannot open the file\" & \"%s\" \n" eaf--buffer-url)
-             "		return false\n"
-             "	end try\n"
+             "    try\n"
+     (format "	      open \"%s\" options \"page=%s\"\n" eaf--buffer-url page-number)
+             "	  on error\n"
+     (format "	      display alert \"Cannot open the file\" & \"%s\" \n" eaf--buffer-url)
+             "	      return false\n"
+             "	  end try\n"
              "end tell\n"
              "\n"
              "tell application \"Adobe Acrobat\"\n"
-             "	tell PDF Window 1\n"
-     (format "		goto page %s \n" page-number)
-             "	end tell\n"
+             "	  tell PDF Window 1\n"
+     (format "	      goto page %s \n" page-number)
+             "	  end tell\n"
              "end tell\n"
              ))))
 
