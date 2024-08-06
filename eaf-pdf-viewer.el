9617e3840839ed7a6f6943d51fd76b6851587bc9
hitswint <wgq_hit@126.com>
2024-08-06 10:16:31 +0800

    Fixed eaf-open-pdf-from-history.

--- eaf-pdf-viewer.el
+++ eaf-pdf-viewer.el
@@ -517,17 +517,23 @@ This function works best if paired with a fuzzy search package."
                   "log.txt"))
          (history-pattern "^\\(.+\\)\\.pdf$")
          (history-file-exists (file-exists-p pdf-history-file-path))
+         (eaf-files-opened (mapcar (lambda (buf)
+                                     (buffer-local-value 'eaf--buffer-url buf))
+                                   (eaf--get-eaf-buffers)))
          (history-pdf (completing-read
                        "[EAF/pdf] Search || History: "
-                       (if history-file-exists
-                           (mapcar
-                            (lambda (h) (when (string-match history-pattern h)
-                                      (if (file-exists-p h)
-                                          (format "%s" h))))
-                            (with-temp-buffer (insert-file-contents pdf-history-file-path)
-                                              (split-string (buffer-string) "\n" t)))
-                         (make-directory (file-name-directory pdf-history-file-path) t)
-                         (with-temp-file pdf-history-file-path "")))))
+                       (cl-remove-if (lambda (x)
+                                       (or (null x)
+                                           (member x eaf-files-opened)))
+                                     (if history-file-exists
+                                         (mapcar
+                                          (lambda (h) (when (string-match history-pattern h)
+                                                        (if (file-exists-p h)
+                                                            (format "%s" h))))
+                                          (with-temp-buffer (insert-file-contents pdf-history-file-path)
+                                                            (split-string (buffer-string) "\n" t)))
+                                       (make-directory (file-name-directory pdf-history-file-path) t)
+                                       (with-temp-file pdf-history-file-path ""))))))
     (if history-pdf (eaf-open history-pdf))))
 
 (defun eaf-pdf-delete-invalid-file-record-from-history ()
